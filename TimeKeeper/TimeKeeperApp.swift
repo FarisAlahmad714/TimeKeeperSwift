f0e5c9ab86c7728a36c1a801e3a33c2016d9e8ba
Faris714 <Farisalahmad01@hotmail.com>
2025-03-02 15:18:17 -0800

    Initial Commit

---
+++ TimeKeeper/TimeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeKeeperApp.swift
+//  TimeKeeper
+//
+//  Created by Faris Alahmad on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeKeeperTests/TimeKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeKeeperTests.swift
+//  TimeKeeperTests
+//
+//  Created by Faris Alahmad on 3/2/25.
+//
+
+import Testing
+
+struct TimeKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
