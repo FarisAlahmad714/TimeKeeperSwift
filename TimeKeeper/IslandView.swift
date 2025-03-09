7594f278d7b92ff72d164dff6669df2f833ac151
Faris714 <Farisalahmad01@hotmail.com>
2025-03-09 09:01:23 -0700

    Enhanced visual detail and animations in all environment scenes for the Alarms view page needs a bit more fine tuning to perfection

--- TimeKeeper/IslandView.swift
+++ TimeKeeper/IslandView.swift
@@ -5,12 +5,4 @@
 //  Created by Faris Alahmad on 3/9/25.
 //
 
-import SwiftUI
-struct IslandView: View {
-    var body: some View {
-        Rectangle()
-            .fill(Color.green)
-            .frame(height: 50)
-            .frame(maxWidth: .infinity, alignment: .bottom)
-    }
-}
+
